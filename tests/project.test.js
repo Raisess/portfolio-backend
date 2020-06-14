c258a17b905a4b05dbb0dccfd0e00c8316ba1da9
danillu <48914278+Raisess@users.noreply.github.com>
2020-06-14 14:32:33 -0300

    updated "project" controllers tests

--- tests/project.test.js
+++ tests/project.test.js
@@ -2,12 +2,24 @@ const fb = require('firebase');
 const config = require('../src/firebase/firebase.json');
 fb.initializeApp(config);
 
-const { create } = require('../src/controllers/project.controller');
+const { create, update } = require('../src/controllers/project.controller');
 
-it('create a project :: {} === true', async () => {
+it('create a project :: {} === true', () => {
   return create({
     username: "teste",
     name: "teste",
+    description: "teste",
+    cover: "teste",
+    link: "teste",
+    git: "teste",
+    color: "teste"
+  })
+    .then(bool => expect(bool).toBe(true));
+});
+
+it('update a project :: {} === true', () => {
+  return update('HGWiuhOKQLTJiaX0', 'teste', {
+    name: "teste2",
     description: "teste",
     cover: "teste",
     link: "teste",
