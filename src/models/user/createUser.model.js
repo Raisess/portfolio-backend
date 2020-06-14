51a11feff87debb4d7436b97d2efdf5df00f1b66
danillu <48914278+Raisess@users.noreply.github.com>
2020-06-14 11:19:32 -0300

    added password encrypt to user create model

--- src/models/user/createUser.model.js
+++ src/models/user/createUser.model.js
@@ -1,10 +1,13 @@
 const fb = require('firebase');
 const db = fb.database();
+const md5 = require('md5');
 const hash = require('../../modules/hash');
 
 const id = hash();
 // user schema
 const userSchema = require('../../schemas/user.schema');
+// pass alt
+const passAlt = require('../../passAlt.json');
 
 module.exports = ({ email, avatar, username, password }) => {
   return db.ref(`/users/${id}`)
@@ -13,7 +16,7 @@ module.exports = ({ email, avatar, username, password }) => {
       email,
       avatar,
       username,
-      password
+      md5(password) + passAlt[0]
     ))
     .then(() => true)
     .catch(() => false);
