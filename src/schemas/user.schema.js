474eb54267fe8a92c5afd134412911be7147c5c6
danillu <48914278+Raisess@users.noreply.github.com>
2020-06-14 10:48:10 -0300

    updated user schema

--- src/schemas/user.schema.js
+++ src/schemas/user.schema.js
@@ -1,8 +1,9 @@
-module.exports = ({ hash, username, password }) => {
+module.exports = ({ hash, email, username, password }) => {
   return {
     crated_at: new Date().toDateString(),
     update_at: new Date().toDateString(),
     id: hash,
+    email: email,
     username: username,
     password: password
   };
