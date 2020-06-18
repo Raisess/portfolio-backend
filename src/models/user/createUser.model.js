3513532ad66e90f3e2fd24b9123bae85e69b9076
danillu <48914278+Raisess@users.noreply.github.com>
2020-06-18 14:08:12 -0300

    implemented uploadImg service to createUser model to set only avatar link to database

--- src/models/user/createUser.model.js
+++ src/models/user/createUser.model.js
@@ -8,8 +8,15 @@ const userSchema = require('../../schemas/user.schema');
 // pass alt
 const passAlt = require('../../passAlt.json');
 
+// imgur upload service
+const uploadImg = require('../../services/imgur_api');
+
 module.exports = ({ email, avatar, username, name, github, password }) => {
   const id = hash();
+
+  const avatarLink = uploadImg(avatar)
+    .then(res => res[1])
+    .catch(() => 'https://assets.newglue.com/assets/avatar_placeholder-c4a9963ad86c68649100b476add586667aaaf4672a3dbfd6abf0e7338f4f5337.jpg');
 
   return db.ref('/users')
     .once('value')
@@ -27,7 +34,7 @@ module.exports = ({ email, avatar, username, name, github, password }) => {
           id,
           md5(email + username + passAlt[0]),
           email,
-          avatar,
+          avatarLink,
           username,
           name,
           `https://github.com/${github}`,
