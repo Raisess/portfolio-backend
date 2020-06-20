12a4b4d773d35928d09eef8a9ab695d2c5d42650
danillu <48914278+Raisess@users.noreply.github.com>
2020-06-20 12:11:35 -0300

    added a checker if the img is base64 or a link in the imgur upload service module

--- src/services/imgur_api/index.js
+++ src/services/imgur_api/index.js
@@ -1,11 +1,28 @@
 const imgur = require('imgur');
+const isBase64 = require('is-base64');
+
+// credencials data
 const { client_id, email, password } = require('./credencials.json');
 
 imgur.setCredentials(email, password, client_id);
 
 // upload image
 module.exports = img => {
-  return imgur.uploadBase64(img)
+  if (isBase64(img)) {
+    return imgur.uploadBase64(img)
+      .then(res => {
+        console.log(res.data.link);
+
+        return [true, res.data.link];
+      })
+      .catch(err => {
+        console.error(err.message);
+
+        return [false];
+      });
+  }
+
+  return imgur.uploadUrl(img)
     .then(res => {
       console.log(res.data.link);
 
